b7496365ef6a06520b72954d67edfda15a722968
ordmoj <hassi@norlens.net>
2018-12-29 22:25:59 -0500

    Adding comments.

--- q2_calling_sense_func.py
+++ q2_calling_sense_func.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-# Qiskit
+# Start by importing and simplifying required modules. 
+
 from sense_hat import SenseHat
 hat = SenseHat()
 
+# Define the execute function that is called by the main controller.
 def execute():
     from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
     from qiskit import execute
     import numpy as np
-    #Set number of bits and number of shots
+    # Set number of bits and number of shots
     n = 2
     print(n)
     sh = 1024
@@ -20,13 +22,12 @@ def execute():
     # Add gates to the circuit
     circuit.h(qr[0])
     circuit.h(qr[1])
-    #circuit.cx(qr[0], qr[1])
     circuit.measure(qr[0], cr[0])
     circuit.measure(qr[1], cr[1])
     # Set the backend to execute on
     from qiskit import Aer
     backend = Aer.get_backend('qasm_simulator')
-    # Create a Quantum Program for a 1024 shot execution of the circuit on the selected backend
+    # Create a Quantum Program for execution of the circuit on the selected backend
     job = execute(circuit, backend, shots=sh)
     # Get the result of the execution
     result = job.result()
@@ -34,6 +35,10 @@ def execute():
     print ("Results:")
     #print (result)
     Qdictres = result.get_counts(circuit)
+    # Print the resulting quantum dictionary.
     print(Qdictres)
+    
+    # Import the Raspberry PI SenseHat display function.
     from qc_sensehat_func import SenseDisplay
+    # Display the quantum dictionary as a bar graph on the SenseHat 8x8 pixel display by calling the SenseDisplay function.
     SenseDisplay(Qdictres,n)
